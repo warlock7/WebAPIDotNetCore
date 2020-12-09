5a89d8108fccaded5b88ac5addb51a4c4a2bd36f
George Kurian <george.kurian@speridian.com>
2020-12-09 13:08:34 +0530

    pushing startup changes

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace WebAPIDotNetCore
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World! API");
                 });
             });
         }
