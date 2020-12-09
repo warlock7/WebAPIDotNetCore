88a1f846a1e2c0359288bd89ca43068735e96cfd
George Kurian <george.kurian@speridian.com>
2020-12-09 13:20:42 +0530

    commit master strtup

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace WebAPIDotNetCore
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World! - API develop");
+                    await context.Response.WriteAsync("Hello World! - API develop changed master");
                 });
             });
         }
